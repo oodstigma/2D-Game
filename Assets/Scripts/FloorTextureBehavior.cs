55a9d072309a84768096bb81ce141daa915d7513
Matthew Miller <oodstigma@gmail.com>
2016-08-27 23:23:34 -0500

    Initial Commit

---
+++ Assets/Scripts/FloorTextureBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTextureBehavior : MonoBehaviour {
+
+    public Sprite corner;
+    public Sprite vertical;
+    public Sprite horizontal;
+
+
+	void Start () {
+	
+	}
+	void Update () {
+	
+	}
+}
